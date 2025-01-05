8c7ce351e973f1688a87ec8f928e2e0a68665ab5
Dima <dmytro.moroz1@gmail.com>
2025-01-05 16:10:40 +0100

    make Card hashable, add ColorsPiles methods to calc final value and get last card

--- src/card.py
+++ src/card.py
@@ -1,13 +1,5 @@
-from enum import Enum
+from src.enums import Color
 from dataclasses import dataclass
-
-
-class Color(Enum):
-    RED = "Red"
-    BLUE = "Blue"
-    YELLOW = "Yellow"
-    GREEN = "Green"
-    WHITE = "White"
 
 
 @dataclass
@@ -15,19 +7,21 @@ class Card:
     color: Color
     value: int
 
+    def __hash__(self) -> int:
+        return hash((self.color, self.value))
+
+    def __eq__(self, other) -> bool:
+        if self.color == other.color and self.value == other.value:
+            return True
+        return False
+
 
 class ColorsPiles:
     def __init__(self):
-        self.piles: dict[Color, list] = {
-            color: list()
-            for color in Color
-        }
+        self.piles: dict[Color, list] = {color: list() for color in Color}
 
     def empty_piles(self):
-        self.piles: dict[Color, list] = {
-            color: list()
-            for color in Color
-        }
+        self.piles: dict[Color, list] = {color: list() for color in Color}
 
     def push(self, card: Card):
         self.piles[card.color].append(card)
@@ -37,3 +31,19 @@ class ColorsPiles:
 
     def is_color_empty(self, color: Color) -> bool:
         return not self.piles[color]
+
+    def get_last_card(self, color: Color) -> Card:
+        return self.piles[color][-1]
+
+    def get_piles_value(self):
+        return sum([self.get_pile_value(color) for color in Color])
+
+    def get_pile_value(self, color: Color) -> int:
+        if self.is_color_empty(color):
+            return 0
+        value = sum([card.value for card in self.piles[color]])
+        value -= 20
+        value *= len([1 for card in self.piles[color] if card.value == 0]) + 1
+        if len(self.piles[color]) >= 8:
+            value += 20
+        return value
